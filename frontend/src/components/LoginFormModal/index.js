8c5057f97ac8608194d70aa5bd5780c48f7eb129
Will Kee <william.w.kee@gmail.com>
2022-03-04 14:11:25 -0700

    added package.json to root

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -7,7 +7,12 @@ const LoginFormModal = () => {
 
     return (
         <>
-            <button onClick={() => setShowModal(true)}>Log In</button>
+            <button
+                className="nav-link login-link"
+                onClick={() => setShowModal(true)}
+            >
+                Log In
+            </button>
             {showModal && (
                 <Modal onClose={() => setShowModal(false)}>
                     <LoginForm />
