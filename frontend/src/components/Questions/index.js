9224a1e5949f94d1a2fb526aa9b852934bc66a3a
Will Kee <william.w.kee@gmail.com>
2022-03-13 16:37:31 -0600

    fixed some css styling

--- frontend/src/components/Questions/index.js
+++ frontend/src/components/Questions/index.js
@@ -29,54 +29,56 @@ const QuestionList = () => {
         isLoaded && (
             <div className="question-container">
                 {questionsReversed.map((question, idx) => (
-                    <Link
-                        key={idx}
-                        className="link-to-single-question"
-                        to={`/questions/${question.id}`}
-                    >
-                        <div
-                            className={
-                                sessionUser
-                                    ? sessionUser.id === question.ownerId
-                                        ? "question-box question-owner-main"
-                                        : "question-box"
-                                    : "question-box"
-                            }
+                    <div className="single-question-outer-container-1">
+                        <Link
+                            key={idx}
+                            className="link-to-single-question"
+                            to={`/questions/${question.id}`}
                         >
-                            <div className="question-profile">
-                                <i className="fa-solid fa-user question"></i>
-                                <div className="profile-desc">
-                                    <div
-                                        className={
-                                            sessionUser
-                                                ? sessionUser.username ===
-                                                  question.User.username
-                                                    ? "question-username-owner"
+                            <div
+                                className={
+                                    sessionUser
+                                        ? sessionUser.id === question.ownerId
+                                            ? "question-box question-owner-main"
+                                            : "question-box"
+                                        : "question-box"
+                                }
+                            >
+                                <div className="question-profile">
+                                    <i className="fa-solid fa-user question"></i>
+                                    <div className="profile-desc">
+                                        <div
+                                            className={
+                                                sessionUser
+                                                    ? sessionUser.username ===
+                                                      question.User.username
+                                                        ? "question-username-owner"
+                                                        : "question-username"
                                                     : "question-username"
-                                                : "question-username"
-                                        }
-                                    >
-                                        {question.User.username}
-                                    </div>
-                                    <div className="question-date">
-                                        {new Date(question.createdAt)
-                                            .toDateString()
-                                            .slice(4)}
+                                            }
+                                        >
+                                            {question.User.username}
+                                        </div>
+                                        <div className="question-date">
+                                            {new Date(question.createdAt)
+                                                .toDateString()
+                                                .slice(4)}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
 
-                            <div className="question-title">
-                                {question.title}
-                            </div>
+                                <div className="question-title">
+                                    {question.title}
+                                </div>
 
-                            <div className="num-answer-text">
-                                {question.Answers.length === 1
-                                    ? `${question.Answers.length} Answer`
-                                    : `${question.Answers.length} Answers`}
+                                <div className="num-answer-text">
+                                    {question.Answers.length === 1
+                                        ? `${question.Answers.length} Answer`
+                                        : `${question.Answers.length} Answers`}
+                                </div>
                             </div>
-                        </div>
-                    </Link>
+                        </Link>
+                    </div>
                 ))}
             </div>
         )
