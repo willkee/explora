678ed32247bcdd9c0458e40c4cb3e55a046bf941
Will Kee <william.w.kee@gmail.com>
2022-11-10 14:41:47 -0700

    1

--- backend/faker-seed.js
+++ backend/faker-seed.js
@@ -2,102 +2,162 @@ const { faker } = require("@faker-js/faker");
 const bcrypt = require("bcryptjs");
 
 const userSeed = (num) => {
-    let i = 0;
+	let i = 0;
 
-    while (i < num) {
-        const user = {
-            email: faker.internet.email(),
-            username: faker.internet.userName(),
-            hashedPassword: bcrypt.hashSync(faker.internet.password()),
-            createdAt: faker.date
-                .between("2020-01-01", "2021-12-31")
-                .toDateString(),
-            updatedAt: faker.date
-                .between("2022-01-01", "2022-03-07")
-                .toDateString(),
-        };
+	while (i < num) {
+		const user = {
+			email: faker.internet.email(),
+			username: faker.internet.userName(),
+			hashedPassword: bcrypt.hashSync(faker.internet.password()),
+			createdAt: faker.date
+				.between("2020-01-01", "2021-12-31")
+				.toDateString(),
+			updatedAt: faker.date
+				.between("2022-01-01", "2022-03-07")
+				.toDateString(),
+		};
 
-        console.log(user, ",");
-        i++;
-    }
+		console.log(user, ",");
+		i++;
+	}
 };
 
 // userSeed(4);
 
 function randomNum(max) {
-    return Math.ceil(Math.random() * max);
+	return Math.ceil(Math.random() * max);
 }
 const questionSeed = (num) => {
-    let i = 0;
+	let i = 0;
 
-    while (i < num) {
-        const id = randomNum(30);
-        const question = {
-            ownerId: id,
-            title: faker.lorem.sentence(),
-            description: faker.lorem.paragraph(),
-            createdAt: faker.date
-                .between("2020-01-01", "2021-12-31")
-                .toDateString(),
-            updatedAt: faker.date
-                .between("2022-01-01", "2022-03-07")
-                .toDateString(),
-        };
-        console.log(question, ",");
-        i++;
-    }
+	while (i < num) {
+		const id = randomNum(30);
+		const question = {
+			ownerId: id,
+			title: faker.lorem.sentence(),
+			description: faker.lorem.paragraph(),
+			createdAt: faker.date
+				.between("2020-01-01", "2021-12-31")
+				.toDateString(),
+			updatedAt: faker.date
+				.between("2022-01-01", "2022-03-07")
+				.toDateString(),
+		};
+		console.log(question, ",");
+		i++;
+	}
 };
 
 // questionSeed(100);
 // console.log(randomNum(28));
 
 const answerSeed = (num) => {
-    let i = 0;
+	let i = 0;
 
-    while (i < num) {
-        const qid = randomNum(200);
-        const uid = randomNum(30);
-        const fDate = faker.date
-            .between("2021-05-01", "2021-12-31")
-            .toDateString();
+	while (i < num) {
+		const qid = randomNum(200);
+		const uid = randomNum(30);
+		const fDate = faker.date
+			.between("2021-05-01", "2021-12-31")
+			.toDateString();
 
-        const answer = {
-            userId: uid,
-            questionId: qid,
-            answer: faker.lorem.paragraph(),
-            createdAt: fDate,
-            updatedAt: fDate,
-        };
-        console.log(answer, ",");
-        i++;
-    }
+		const answer = {
+			userId: uid,
+			questionId: qid,
+			answer: faker.lorem.paragraph(),
+			createdAt: fDate,
+			updatedAt: fDate,
+		};
+		console.log(answer, ",");
+		i++;
+	}
 };
 
 // answerSeed(200);
 
 const votePairs = [];
 const upvoteSeed = (num) => {
-    let i = 0;
+	let i = 0;
 
-    while (i < num) {
-        const userId = randomNum(30);
-        const answerId = randomNum(400);
-        const duplicate = votePairs.find(
-            (vote) => vote.answerId === answerId && vote.userId === userId
-        );
-        if (duplicate) {
-            continue;
-        }
-        const votes = {
-            userId,
-            answerId,
-            createdAt: "new Date()",
-            updatedAt: "new Date()",
-        };
-        console.log(votes, ",");
-        votePairs.push(votes);
-        i++;
-    }
+	while (i < num) {
+		const userId = randomNum(30);
+		const answerId = randomNum(400);
+		const duplicate = votePairs.find(
+			(vote) => vote.answerId === answerId && vote.userId === userId
+		);
+		if (duplicate) {
+			continue;
+		}
+		const votes = {
+			userId,
+			answerId,
+			createdAt: "new Date()",
+			updatedAt: "new Date()",
+		};
+		console.log(votes, ",");
+		votePairs.push(votes);
+		i++;
+	}
 };
 
 // upvoteSeed(500);
+function randomNum(max) {
+	return Math.ceil(Math.random() * max);
+}
+
+const ownerNum = (num) => {
+	i = 0;
+	while (i < num) {
+		console.log(randomNum(25));
+		i++;
+	}
+};
+// ownerNum(91);
+
+// const phoneGenerator = (num) => {
+// 	i = 0;
+
+// 	while (i < num) {
+// 		const mid3 = 111 + randomNum(888);
+// 		const last4 = 1000 + randomNum(8999);
+// 		const phoneNum = `212-${mid3}-${last4}`;
+// 		console.log(phoneNum);
+// 		i++;
+// 	}
+// };
+// phoneGenerator(100);
+
+const userFaker = (num) => {
+	i = 0;
+
+	while (i < num) {
+		const obj = {};
+		date = faker.date.between("2020-01-01", "2021-12-31").toDateString();
+		obj.first_name = faker.name.firstName();
+		obj.last_name = faker.name.lastName();
+		obj.email = faker.internet.email();
+		obj.hashed_password = bcrypt.hashSync(faker.internet.password());
+		obj.business_owner = "False";
+		obj.created_at = date;
+		obj.updated_at = date;
+		console.log(obj);
+		i++;
+	}
+};
+
+const otherFaker = (num) => {
+	i = 0;
+
+	while (i < num) {
+		const obj = {};
+		date = faker.date.between("2022-01-01", "2022-04-01").toDateString();
+		obj.type = "";
+		obj.created_at = date;
+		obj.updated_at = date;
+		console.log(obj);
+		i++;
+	}
+};
+
+// otherFaker(20);
+console.log(bcrypt.hashSync("password"));
--- backend/routes/api/questions.js
+++ backend/routes/api/questions.js
@@ -9,123 +9,123 @@ const { Question, Answer, User } = require("../../db/models");
 const router = express.Router();
 
 const validateQuestion = [
-    check("title")
-        .exists({ checkFalsy: true })
-        .withMessage("Please enter a title.")
-        .isLength({ min: 3, max: 255 })
-        .withMessage("Please enter a title between 3 and 255 characters."),
-    handleValidationErrors,
+	check("title")
+		.exists({ checkFalsy: true })
+		.withMessage("Please enter a title.")
+		.isLength({ min: 3, max: 255 })
+		.withMessage("Please enter a title between 3 and 255 characters."),
+	handleValidationErrors,
 ];
 
 router.get(
-    "/",
-    asyncHandler(async (req, res) => {
-        const questions = await Question.findAll({
-            include: [{ model: Answer }, { model: User }],
-            order: [["id"]],
-        });
-        return res.json({ questions });
-    })
+	"/",
+	asyncHandler(async (req, res) => {
+		const questions = await Question.findAll({
+			include: [{ model: Answer }, { model: User }],
+			order: [["id"]],
+		});
+		return res.json({ questions });
+	})
 );
 
 router.post(
-    "/",
-    restoreUser,
-    validateQuestion,
-    asyncHandler(async (req, res) => {
-        const { title, description } = req.body;
-        const { user } = req;
+	"/",
+	restoreUser,
+	validateQuestion,
+	asyncHandler(async (req, res) => {
+		const { title, description } = req.body;
+		const { user } = req;
 
-        const questionX = await Question.create({
-            ownerId: user.id,
-            title,
-            description,
-        });
-        const newQuestion = await Question.findOne({
-            include: [{ model: Answer }, { model: User }],
-            where: { id: questionX.id },
-        });
+		const questionX = await Question.create({
+			ownerId: user.id,
+			title,
+			description,
+		});
+		const newQuestion = await Question.findOne({
+			include: [{ model: Answer }, { model: User }],
+			where: { id: questionX.id },
+		});
 
-        return res.json(newQuestion);
-    })
+		return res.json(newQuestion);
+	})
 );
 
 router.get(
-    "/:questionId",
-    asyncHandler(async (req, res) => {
-        const questionId = parseInt(req.params.questionId, 10);
+	"/:questionId",
+	asyncHandler(async (req, res) => {
+		const questionId = parseInt(req.params.questionId, 10);
 
-        const selectedQuestion = await Question.findOne({
-            include: [
-                { model: User },
-                { model: Answer, include: { model: User } },
-            ],
-            where: { id: questionId },
-        });
-        return res.json(selectedQuestion);
-    })
+		const selectedQuestion = await Question.findOne({
+			include: [
+				{ model: User },
+				{ model: Answer, include: { model: User } },
+			],
+			where: { id: questionId },
+		});
+		return res.json(selectedQuestion);
+	})
 );
 
 router.put(
-    "/:questionId",
-    validateQuestion,
-    asyncHandler(async (req, res) => {
-        const questionId = parseInt(req.params.questionId, 10);
+	"/:questionId",
+	validateQuestion,
+	asyncHandler(async (req, res) => {
+		const questionId = parseInt(req.params.questionId, 10);
 
-        const selectedQuestion = await Question.findOne({
-            where: { id: questionId },
-        });
+		const selectedQuestion = await Question.findOne({
+			where: { id: questionId },
+		});
 
-        try {
-            if (selectedQuestion) {
-                await Question.update(req.body, {
-                    where: { id: questionId },
-                    returning: true,
-                    plain: true,
-                });
+		try {
+			if (selectedQuestion) {
+				await Question.update(req.body, {
+					where: { id: questionId },
+					returning: true,
+					plain: true,
+				});
 
-                const question = await Question.findByPk(questionId, {
-                    include: [
-                        { model: User },
-                        { model: Answer, include: { model: User } },
-                    ],
-                });
+				const question = await Question.findByPk(questionId, {
+					include: [
+						{ model: User },
+						{ model: Answer, include: { model: User } },
+					],
+				});
 
-                return res.json(question);
-            }
-        } catch (err) {
-            console.error("Error - Question Not Found: ", err);
-        }
-    })
+				return res.json(question);
+			}
+		} catch (err) {
+			console.error("Error - Question Not Found: ", err);
+		}
+	})
 );
 
 router.delete(
-    "/:questionId",
-    asyncHandler(async (req, res) => {
-        const paramId = parseInt(req.params.questionId, 10);
-        const question = await Question.findByPk(paramId);
+	"/:questionId",
+	asyncHandler(async (req, res) => {
+		const paramId = parseInt(req.params.questionId, 10);
+		const question = await Question.findByPk(paramId);
 
-        const isEmpty = (obj) => Object.keys(obj).length === 0;
+		const isEmpty = (obj) => Object.keys(obj).length === 0;
 
-        if (question) {
-            const id = question.id;
+		if (question) {
+			const id = question.id;
 
-            const relatedAnswers = await Answer.findAll({
-                where: { questionId: id },
-            });
+			const relatedAnswers = await Answer.findAll({
+				where: { questionId: id },
+			});
 
-            if (!isEmpty(relatedAnswers)) {
-                await Answer.destroy({
-                    where: { questionId: id },
-                });
-            }
+			if (!isEmpty(relatedAnswers)) {
+				await Answer.destroy({
+					where: { questionId: id },
+				});
+			}
 
-            await Question.destroy({ where: { id } });
-            return res.json({ id });
-        } else {
-            throw new Error("Question cannot be found.");
-        }
-    })
+			await Question.destroy({ where: { id } });
+			return res.json({ id });
+		} else {
+			throw new Error("Question cannot be found.");
+		}
+	})
 );
 
 module.exports = router;
