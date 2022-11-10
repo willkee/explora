45193cb17cac9aa337f4157a7c0ca4a150a46d7e
Will Kee <william.w.kee@gmail.com>
2022-11-10 15:11:39 -0700

    initial setup

--- backend/db/migrations/20220303041718-create-user.js
+++ backend/db/migrations/20220303041718-create-user.js
@@ -1,40 +1,50 @@
 "use strict";
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+	options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
-    up: (queryInterface, Sequelize) => {
-        return queryInterface.createTable("Users", {
-            id: {
-                allowNull: false,
-                autoIncrement: true,
-                primaryKey: true,
-                type: Sequelize.INTEGER,
-            },
-            username: {
-                unique: true,
-                allowNull: false,
-                type: Sequelize.STRING(30),
-            },
-            email: {
-                unique: true,
-                allowNull: false,
-                type: Sequelize.STRING(256),
-            },
-            hashedPassword: {
-                allowNull: false,
-                type: Sequelize.STRING.BINARY,
-            },
-            createdAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.fn("now"),
-            },
-            updatedAt: {
-                allowNull: false,
-                type: Sequelize.DATE,
-                defaultValue: Sequelize.fn("now"),
-            },
-        });
-    },
-    down: (queryInterface, Sequelize) => {
-        return queryInterface.dropTable("Users");
-    },
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable(
+			"Users",
+			{
+				id: {
+					allowNull: false,
+					autoIncrement: true,
+					primaryKey: true,
+					type: Sequelize.INTEGER,
+				},
+				username: {
+					unique: true,
+					allowNull: false,
+					type: Sequelize.STRING(30),
+				},
+				email: {
+					unique: true,
+					allowNull: false,
+					type: Sequelize.STRING(256),
+				},
+				hashedPassword: {
+					allowNull: false,
+					type: Sequelize.STRING.BINARY,
+				},
+				createdAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.fn("now"),
+				},
+				updatedAt: {
+					allowNull: false,
+					type: Sequelize.DATE,
+					defaultValue: Sequelize.fn("now"),
+				},
+			},
+			options
+		);
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable("Users", options);
+	},
 };
